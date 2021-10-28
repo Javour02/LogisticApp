2d61d95b1468f8fa3b0b61e6295135e9653a89bc
Javour02 <javiergach@unisabana.edu.co>
2021-10-27 23:15:12 -0500

    ola

--- app/src/main/java/com/example/logisticapp/Login.java
+++ app/src/main/java/com/example/logisticapp/Login.java
@@ -4,12 +4,40 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class Login extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+    }
+
+
+    public void loggearse(View view) {
+        db.collection("users")
+            .get()
+            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+
+                        }
+                    } else {
+
+                    }
+                }
+            });
     }
 }
